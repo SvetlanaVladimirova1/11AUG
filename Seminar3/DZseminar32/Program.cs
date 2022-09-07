2a4743d0eae7bf61f087a56bc26be156734794b6
Svetlana <tina31@rambler.ru>
2022-09-07 18:13:36 +0300

    DZ33

--- Seminar3/DZseminar32/Program.cs
+++ Seminar3/DZseminar32/Program.cs
@@ -3,3 +3,19 @@
 //12821 -> да
 //23432 -> да
 
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+string result = Convert.ToString(num);
+if (num>=10000 && num<100000)
+{
+    if (result[0] == result[4] && result[1] == result[3])
+    {
+         Console.WriteLine("да");     
+    }
+    else
+    {
+         Console.WriteLine("нет");
+    }   
+
+}
+
