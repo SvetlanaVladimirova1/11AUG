5e25c7b9ca5ee108be07c822650094901d2879b9
Svetlana <tina31@rambler.ru>
2022-09-07 18:24:08 +0300

    DZ41

---
+++ Seminar4/DZseminar41/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число степени a");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(Math.Pow(n,a));
